dfd612a7e48b67ce6c75c352a954d0047eab56f0
threeanra <trian.radis448@gmail.com>
2024-02-22 02:31:02 +0700

    Fix the folder structure

---
+++ K-for-Kotlin/src/main/kotlin/com/loki/k0tlin/Property-Delegation/Main.kt
@@ -0,0 +1,35 @@
+package com.loki.k0tlin.`Property-Delegation`
+
+import kotlin.reflect.KProperty
+
+//This method avoids redundancy
+class DelegateGenericClass {
+    private var value: Any = "Default"
+
+    operator fun getValue(classRef: Any, property: KProperty<*>): Any {
+        println("Fungsi ini sama seperti getter untuk properti ${property.name} pada class $classRef")
+        return value
+    }
+
+    operator fun setValue(classRef: Any, property: KProperty<*>, newValue: Any) {
+        println("Nilai ${property.name} dari: $value akan berubah menjadi $newValue")
+        value = newValue
+    }
+}
+
+class Animal {
+    var name: Any by DelegateGenericClass()
+    var weight: Any by DelegateGenericClass()
+    var age: Any by DelegateGenericClass()
+}
+
+fun main(){
+    val animal = Animal()
+    animal.name = "Penaldo"
+    animal.weight = 5.6
+    animal.age = 2
+
+    println("Nama: ${animal.name}")
+    println("Berat: ${animal.weight}")
+    println("Umur: ${animal.age} Tahun")
+}
