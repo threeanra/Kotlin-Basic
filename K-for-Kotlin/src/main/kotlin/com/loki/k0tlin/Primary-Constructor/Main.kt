82d2f626bc8dd0144f79713b0e7a1a411f7b4974
threeanra <trian.radis448@gmail.com>
2024-02-22 02:30:02 +0700

    Fix the folder structure

---
+++ K-for-Kotlin/src/main/kotlin/com/loki/k0tlin/Primary-Constructor/Main.kt
@@ -0,0 +1,20 @@
+package com.loki.k0tlin.`Primary-Constructor`
+
+class Animal(name: String, weight: Double, age: Int, isMammal: Boolean) {
+    val name: String
+    val weight: Double
+    val age: Int
+    val isMammal: Boolean
+
+    init {
+        this.weight = if(weight < 0) 0.1 else weight
+        this.age = if(age < 0) 0  else age
+        this.name = name
+        this.isMammal = isMammal
+    }
+}
+
+fun main() {
+    val dicodingCat = Animal("Pessi", 4.2, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}")
+}
